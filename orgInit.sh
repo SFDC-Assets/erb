281ce8f772392366fbcc4421b1d1919a0ef3e6eb
Ed Sandoval <esandoval@salesforce.com>
2021-07-09 15:59:09 +0100

    wait for up to 6 mins

--- orgInit.sh
+++ orgInit.sh
@@ -4,6 +4,6 @@ sfdx force:user:password:generate
 
 sfdx force:data:bulk:upsert -s Account -f data/accounts.csv -i External_Id__c -w 3
 sfdx force:data:bulk:upsert -s Product2 -f data/products.csv -i External_Id__c -w 3
-sfdx force:data:bulk:upsert -s Purchase_History__c -f data/purchase-history.csv -i External_Id__c -w 3
+sfdx force:data:bulk:upsert -s Purchase_History__c -f data/purchase-history.csv -i External_Id__c -w 6
 
 sfdx force:org:open
