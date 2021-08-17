a67e7eec031ab44442426cc88069ada62b7d35a7
Jonathan Zhou <jonathan.zhou@jzhou-ltm1.internal.salesforce.com>
2021-08-16 19:29:07 -0700

    automig loading and updated init script

--- orgInit.sh
+++ orgInit.sh
@@ -1,8 +1,10 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 5
+sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 5 --userprefix einstein -o recommender.workshop
 sfdx force:source:push
-sfdx force:user:password:generate
-sfdx force:data:bulk:upsert -s Account -f data/accounts.csv -i External_Id__c -w 3
-sfdx force:data:bulk:upsert -s Product2 -f data/products.csv -i External_Id__c -w 3
-sfdx force:data:bulk:upsert -s Purchase_History__c -f data/purchase-history.csv -i External_Id__c -w 10
+sfdx shane:user:password:set -g User -l User -p salesforce1
+
+sfdx automig:load -d ./automig
+# sfdx force:data:bulk:upsert -s Account -f data/accounts.csv -i External_Id__c -w 3
+# sfdx force:data:bulk:upsert -s Product2 -f data/products.csv -i External_Id__c -w 3
+# sfdx force:data:bulk:upsert -s Purchase_History__c -f data/purchase-history.csv -i External_Id__c -w 10
 sfdx force:org:open
 
